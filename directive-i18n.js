f3a6d45e2929684bea142a16ecca41af1e866708
Sergey Bokatuk <siarhei.bakatsiuk@gmail.com>
2017-04-07 21:11:29 +0300

    no message

--- directive-i18n.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-angular.module('sspApp').directive('sspi18n',function(){
-  return {
-    restict: 'EA',
-    link: function(scope, element, attrs){
-      element.replaceWith(i18n[element.text()]);
-    }
-  };
-});
